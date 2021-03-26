457b564160d7ca6eff938f6e8e3e75420f4bf025
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2021-03-26 20:47:14 +0300

    add generate qrcodes from file

--- src/App.vue
+++ src/App.vue
@@ -8,39 +8,55 @@
     <v-main>
       <v-container>
         <v-tabs v-model="tab" class="mt-12">
-          <v-tab>Import from file</v-tab>
+          <v-tab :disabled="Boolean(form.data.adress || form.data.key)">Import from file</v-tab>
           <h2 class="pl-10 pr-10">OR</h2>
-          <v-tab>Enter manually</v-tab>
+          <v-tab :disabled="Boolean(file)">Enter manually</v-tab>
         </v-tabs>
         <v-tabs-items v-model="tab">
           <v-tab-item class="mt-10">
-            <v-file-input label="File input" accept=".csv" outlined dense></v-file-input>
+            <v-file-input
+              v-model="file"
+              label="File input"
+              accept=".csv"
+              outlined
+              dense
+              @change="extractDataFromFile()"
+            ></v-file-input>
+            <v-btn depressed @click="reset()" color="red" dark class="mr-4">Reset</v-btn>
+            <v-btn depressed @click="generateFromFile()" color="green" dark>
+              Generate QRs and PDF
+            </v-btn>
           </v-tab-item>
           <v-tab-item class="mt-10">
             <v-text-field
-              v-model="adress"
+              v-model="form.data.adress"
               label="Adress"
-              :rules="[rules.required]"
+              :rules="[form.rules.required]"
               outlined
             ></v-text-field>
             <v-text-field
-              v-model="key"
-              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-              :rules="[rules.required]"
-              :type="show1 ? 'text' : 'password'"
+              v-model="form.data.key"
+              :append-icon="form.show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[form.rules.required]"
+              :type="form.show1 ? 'text' : 'password'"
               label="Key"
               outlined
-              @click:append="show1 = !show1"
+              @click:append="form.show1 = !form.show1"
             ></v-text-field>
+            <v-btn depressed @click="reset()" color="red" dark class="mr-4">Reset</v-btn>
+            <v-btn depressed @click="generateFromInputs()" color="green" dark>
+              Generate QRs and PDF
+            </v-btn>
           </v-tab-item>
         </v-tabs-items>
-        <v-btn depressed @click="reset()" color="red" dark class="mr-4">Reset</v-btn>
-        <v-btn depressed @click="genereateCodes()" color="green" dark>Generate QRs and PDF</v-btn>
-        <div class="d-flex justify-space-between mt-12">
+        <div
+          v-if="Boolean(form.data.adress || form.data.key)"
+          class="d-flex justify-space-between mt-12"
+        >
           <v-card outline class="pa-4" width="45%">
             <h3 class="text-center title">
               Adress <br />
-              {{ adress }}
+              {{ form.data.adress }}
             </h3>
             <canvas class="mx-auto d-block" id="adressQRcode"></canvas>
           </v-card>
@@ -48,15 +64,37 @@
             <h3 class="text-center title">
               Key <br />
               <input
-                v-model="key"
+                v-model="form.data.key"
                 disabled
-                :type="show1 ? 'text' : 'password'"
+                :type="form.show1 ? 'text' : 'password'"
                 style="width: 100%"
                 class="text-center"
               />
             </h3>
             <canvas class="mx-auto d-block" id="keyQRcode"></canvas>
           </v-card>
+        </div>
+        <div v-if="fileData.length">
+          <div
+            v-for="(item, index) in fileData"
+            :key="item.key"
+            class="d-flex justify-space-between mt-12"
+          >
+            <v-card outline class="pa-4" width="45%">
+              <h3 class="text-center title">
+                Adress <br />
+                {{ item.adress }}
+              </h3>
+              <canvas class="mx-auto d-block" :id="`adressQRcode${index}`"></canvas>
+            </v-card>
+            <v-card outline class="pa-4" width="45%">
+              <h3 class="text-center title">
+                Key <br />
+                {{ item.key }}
+              </h3>
+              <canvas class="mx-auto d-block" :id="`keyQRcode${index}`"></canvas>
+            </v-card>
+          </div>
         </div>
       </v-container>
     </v-main>
@@ -71,14 +109,20 @@ export default {
 
   data: () => ({
     tab: null,
-    adress: '',
-    key: '',
-    show1: false,
-    rules: {
-      required: (value) => !!value || 'Required.',
-      min: (v) => v.length >= 8 || 'Min 8 characters',
-      emailMatch: () => `The email and password you entered don't match`,
+    form: {
+      data: {
+        adress: '',
+        key: '',
+      },
+      show1: false,
+      rules: {
+        required: (value) => !!value || 'Required.',
+        min: (v) => v.length >= 8 || 'Min 8 characters',
+        emailMatch: () => `The email and password you entered don't match`,
+      },
     },
+    file: null,
+    fileData: [],
   }),
   methods: {
     clearQRCodes() {
@@ -89,23 +133,51 @@ export default {
       });
     },
     reset() {
-      this.adress = '';
-      this.key = '';
+      this.form.data.adress = '';
+      this.form.data.key = '';
+      this.file = null;
+      this.fileData = [];
       this.clearQRCodes();
       return false;
     },
-    genereateCodes() {
-      const adressCanvas = document.getElementById('adressQRcode');
-      const keyCanvas = document.getElementById('keyQRcode');
-      QRCode.toCanvas(adressCanvas, this.adress, (error) => {
-        if (error) console.error(error);
-        console.log('success!');
-      });
-      QRCode.toCanvas(keyCanvas, this.key, (error) => {
-        if (error) console.error(error);
-        console.log('success!');
+    // eslint-disable-next-line consistent-return
+    extractDataFromFile() {
+      if (this.file === null) {
+        this.fileData = [];
+        return false;
+      }
+
+      const reader = new FileReader();
+      reader.readAsBinaryString(this.file);
+      reader.onload = () => {
+        const data = reader.result.replace(/\n/g, ',').split(',');
+        data.splice(0, 4);
+
+        for (let i = 0; i < data.length; i += 2) {
+          const obj = {};
+          obj.adress = data[i];
+          obj.key = data[i + 1];
+          this.fileData.push(obj);
+        }
+      };
+    },
+    generateFromFile() {
+      this.fileData.forEach((value, index) => {
+        const adressCanvas = document.querySelector(`#adressQRcode${index}`);
+        const keyCanvas = document.querySelector(`#keyQRcode${index}`);
+        QRCode.toCanvas(adressCanvas, value.adress);
+        QRCode.toCanvas(keyCanvas, value.key);
+        return false;
       });
     },
+    generateFromInputs() {
+      Object.entries(this.form.data).forEach(([key, value]) => {
+        const canvas = document.querySelector(`#${key}QRcode`);
+        QRCode.toCanvas(canvas, value);
+        return false;
+      });
+    },
+    genereateCodes() {},
   },
 };
 </script>
